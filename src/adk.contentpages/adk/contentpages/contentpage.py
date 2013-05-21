2dee3682323469eb696903dad52715d23bba58ff
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-05-21 16:42:20 +0200

    Add basic folderish content type

---
+++ src/adk.contentpages/adk/contentpages/contentpage.py
@@ -0,0 +1,43 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from adk.contentpages import MessageFactory as _
+
+
+class IContentPage(form.Schema, IImageScaleTraversable):
+    """
+    Content page with automatic subcontent listing
+    """
+    displayTitle = schema.TextLine(
+        title=_(u"Display Title"),
+        description=_(u"Alternative Title for displaying alternative "
+                      u"headline"),
+        required=True,
+    )
+    text = RichText(
+        title=_(u"Body Text"),
+        required=True,
+    )
+
+
+class ContentPage(dexterity.Container):
+    grok.implements(IContentPage)
+
+
+class View(grok.View):
+    grok.context(IContentPage)
+    grok.require('zope2.View')
+    grok.name('view')
--- src/adk.contentpages/adk/contentpages/tests.py
+++ src/adk.contentpages/adk/contentpages/tests.py
@@ -52,6 +52,14 @@ def test_suite():
 
         # -*- extra stuff goes here -*-
 
+        # Integration tests for ContentPage
+        ztc.ZopeDocFileSuite(
+            'ContentPage.txt',
+            package='adk.contentpages',
+            optionflags = OPTION_FLAGS,
+            test_class=TestCase),
+
+
         ])
 
 if __name__ == '__main__':
