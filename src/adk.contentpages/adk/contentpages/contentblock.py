6cb17999a0667540c60c97236810176aaf31416c
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-06-05 02:09:04 +0200

    Update block view and refactor cms ui settings

--- src/adk.contentpages/adk/contentpages/contentblock.py
+++ src/adk.contentpages/adk/contentpages/contentblock.py
@@ -1,3 +1,5 @@
+from Acquisition import aq_inner
+from Acquisition import aq_parent
 from five import grok
 from plone.directives import dexterity, form
 
@@ -45,6 +47,16 @@ class View(grok.View):
     grok.context(IContentBlock)
     grok.require('zope2.View')
     grok.name('view')
+
+    def parentpage_url(self):
+        context = aq_inner(self.context)
+        parent = aq_parent(context)
+        return parent.absolute_url()
+
+    def parentpage_title(self):
+        context = aq_inner(self.context)
+        parent = aq_parent(context)
+        return parent.Title()
 
 
 class ContentView(grok.View):
