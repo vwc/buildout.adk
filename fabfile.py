566f50c4b6a6a393ebbe7e63743091a5bf1e0dff
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-06-07 13:17:36 +0200

    Update fabfile and prepare new deploy

--- fabfile.py
+++ fabfile.py
@@ -6,47 +6,81 @@ from fabric.api import run
 from fabric.api import sudo
 
 
-def www():
+def server():
     env.use_ssh_config = True
     env.forward_agent = True
     env.port = '22222'
     env.user = 'root'
-    env.hosts = ['sechszuvier.vorwaerts-werbung.de']
+    env.hosts = ['6zu4']
     env.webserver = '/opt/webserver/buildout.webserver'
     env.code_root = '/opt/sites/adk-staging/buildout.adk'
+    env.sitename = 'isps'
     env.code_user = 'root'
     env.prod_user = 'www'
 
 
 def ls():
+    """ Low level configuration test """
     with cd(env.code_root):
         run('ls')
 
 
-def status():
-    """
-        Find out the running status of the server and deploy.
-    """
-    # General health of the server.
-    run('cat /proc/loadavg')
+def uptime():
+    """ Server uptime """
     run('uptime')
+
+
+def load():
+    """ Server average system load """
+    run('cat /proc/loadavg')
+
+
+def memory():
+    """ Server memory usage """
     run('free')
-    run('df -h')
+
+
+def disk():
+    """ Server disk and filesystem usage """
+    run('df -ha')
+
+
+def supervisor():
+    """ Webserver process status """
     with cd(env.webserver):
         run('bin/supervisorctl status')
 
 
+def status():
+    """ General system status information """
+    # General health of the server.
+    uptime()
+    load()
+    memory()
+    disk()
+    supervisor()
+
+
 def update():
+    """ Update buildout from git/master """
     with cd(env.code_root):
         run('nice git pull')
 
 
-def rebuild():
+def build():
+    """ Run buildout deployment profile """
     with cd(env.code_root):
         run('bin/buildout -Nc deployment.cfg')
 
 
-def instance_restart():
+def build_full():
+    """ Run buildout deployment profile and enforce updates """
+    with cd(env.code_root):
+        run('bin/buildout -c deployment.cfg')
+
+
+def restart():
+    """ Restart instance """
     with cd(env.webserver):
         run('nice bin/supervisorctl restart instance-%(sitename)s' % env)
 
@@ -57,7 +91,27 @@ def supervisorctl(*cmd):
         run('bin/supervisorctl ' + ' '.join(cmd))
 
 
+def prepare_deploy():
+    """ Push committed local changes to git """
+    local('git push')
+
+
 def deploy():
+    """ Deploy current master to production server """
     update()
-    #rebuild()
-    instance_restart()
+    restart()
+
+
+def deploy_full():
+    """ Deploy current master to production and run buildout """
+    prepare_deploy()
+    update()
+    build()
+    restart()
+
+
+def rebuild():
+    """ Deploy current master and run full buildout """
+    update()
+    build_full()
+    restart()
