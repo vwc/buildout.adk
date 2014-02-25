bf9424533fd6cbb639b912588bc0fc48b42854e3
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-25 16:43:01 +0100

    Update translations and fix router

--- src/adk.contentpages/adk/contentpages/approuter.py
+++ src/adk.contentpages/adk/contentpages/approuter.py
@@ -9,10 +9,16 @@ class AppRouter(grok.View):
     grok.require('zope2.View')
     grok.name('app-router')
 
+    def update(self):
+        self.next_url = self._get_redirector()
+
     def render(self):
+        return self.request.response.redirect(self.next_url)
+
+    def _get_redirector(self):
         portal = api.portal.get()
         portal_url = portal.absolute_url()
-        next_url = portal_url + '/de'
         if not api.user.is_anonymous():
-            next_url = portal_url
-        return self.request.response.redirect(next_url)
+            return portal_url
+        else:
+            return portal_url + '/de'
