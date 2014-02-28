1c8c2ca4923f59b60fff7b3d5b28e8552d63fe7d
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-02-28 17:01:08 +0100

    Add standalone testimonial listing view

--- src/adk.testimonials/adk/testimonials/testimonials.py
+++ src/adk.testimonials/adk/testimonials/testimonials.py
@@ -1,4 +1,6 @@
+from Acquisition import aq_inner
 from five import grok
+from plone import api
 
 from z3c.form import group, field
 from zope import schema
@@ -13,6 +15,10 @@ from plone.namedfile.field import NamedImage, NamedFile
 from plone.namedfile.field import NamedBlobImage, NamedBlobFile
 from plone.namedfile.interfaces import IImageScaleTraversable
 
+from plone.app.contentlisting.interfaces import IContentListing
+
+from Products.CMFCore.interfaces import IContentish
+from adk.testimonials.testimonial import ITestimonial
 
 from adk.testimonials import MessageFactory as _
 
@@ -33,3 +39,19 @@ class View(grok.View):
     grok.context(ITestimonials)
     grok.require('zope2.View')
     grok.name('view')
+
+
+class TestimonialListing(grok.View):
+    grok.context(IContentish)
+    grok.require('zope2.View')
+    grok.name('testimonial-listing')
+
+    def update(self):
+        self.has_items = len(self.testimonials()) > 0
+
+    def testimonials(self):
+        catalog = api.portal.get_tool(name="portal_catalog")
+        results = catalog(object_provides=ITestimonial.__identifier__,
+                          review_state='published')
+        items = IContentListing(results)
+        return items
