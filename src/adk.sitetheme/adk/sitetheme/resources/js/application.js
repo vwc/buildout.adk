2e9e2a8f44307787884e6df1c76b8b734a6dc3eb
Christoph Boehner <cb@vorwaerts-werbung.de>
2014-10-20 22:27:31 +0200

    Rebuild theme

--- src/adk.sitetheme/adk/sitetheme/resources/js/application.js
+++ src/adk.sitetheme/adk/sitetheme/resources/js/application.js
@@ -1,18 +1,18 @@
-/*jslint white:false, onevar:true, undef:true, nomen:true, eqeqeq:true, plusplus:true, bitwise:true, regexp:true, newcap:true, immed:true, strict:false, browser:true */
-/*global Modernizr, jQuery:false, document:false */
 'use strict';
-
 (function ($) {
     $(document).ready(function () {
-        if ($('body').hasClass('lt-ie7')) {return; }
-        // Application specific javascript code goes here
-        if (!Modernizr.csstransitions) { // Test if CSS transitions are supported
+        if ($('body').hasClass('lt-ie7')) {
+            return;
+        }
+        if (!Modernizr.csstransitions) {
             $(function () {
                 $('.dim-in').on('load', function () {
-                    $(this).animate({opacity: '1'}, {queue: false, duration: 500});
+                    $(this).animate({ opacity: '1' }, {
+                        queue: false,
+                        duration: 500
+                    });
                 });
             });
         }
-    }
-    );
+    });
 }(jQuery));
